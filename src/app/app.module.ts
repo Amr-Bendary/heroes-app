ba2cd96e79481241ddaff9f2befbac65188c0611
Amr Bendary <76397374+Amr-Bendary@users.noreply.github.com>
2021-04-30 19:08:32 +0200

    Initialization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { TableComponent } from './components/shared/table/table.component';
+import { FiltersComponent } from './components/shared/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProfileComponent,
+    TableComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
